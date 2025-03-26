b67ca1cf313cb220a61ef31a5671b23bca1b8b5f
kai-xlr <xhauma@protonmail.com>
2025-03-25 23:02:34 -0400

    first commit

---
+++ grocery_store.py
@@ -0,0 +1,7 @@
+Quantity = 23
+Price = 1.49
+Review = 82.5
+ReviewDisplay = int(Review // 1)
+Location = "F"
+
+print(f"An apple costs: ${Price}, there are {Quantity} in inventory found in section: {Location} and your customers gave it an average review of {ReviewDisplay}%!")
